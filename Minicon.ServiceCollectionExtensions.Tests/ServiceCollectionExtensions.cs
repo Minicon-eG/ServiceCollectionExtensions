377c4c2459724d1a26a5f65d5861f6f72a01cb94
Michael Nikolaus <nikolaus@gsg-online.com>
2022-08-23 11:16:29 +0200

    Initial Commit

---
+++ Minicon.ServiceCollectionExtensions.Tests/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Moq;
+
+namespace Minicon.ServiceCollectionExtensions.Tests;
+
+public static class ServiceCollectionExtensions
+{
+	public static IServiceCollection AddMockObject<T>(
+		this IServiceCollection services,
+		ServiceLifetime lifetime = ServiceLifetime.Scoped
+	) where T : class
+	{
+		services.Add(
+			new ServiceDescriptor(typeof(T), _ => new Mock<T>().Object, lifetime)
+		);
+
+		return services;
+	}
+}
